d28936b40629b0b178505705a012bb6cf809c708
krmukesha <m.kumar@braininventory.com>
2022-01-16 22:37:45 +0530

    Initial Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,92 @@
+import React from "react";
+import {
+  Badge,
+  Button,
+  Container,
+  Dropdown,
+  FormControl,
+  Nav,
+  Navbar,
+} from "react-bootstrap";
+import { AiFillDelete } from "react-icons/ai";
+import { FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { CartState } from "../context/Context";
+
+
+const Header = () => {
+
+  const {
+    state: { cart },
+    dispatch, productDispatch,
+  } = CartState();
+  return (
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+      <Container>
+        <Navbar.Brand>
+          <Link to="/">Shopping Cart</Link>
+        </Navbar.Brand>
+        <Navbar.Text className="Search">
+          <FormControl
+            style={{ maxWidth: 500 }}
+            placeholder="Search Product"
+            className="m-auto"
+            aria-label="Search"
+            onChange={(e) => {
+              productDispatch({
+                type: "FILTER_BY_SEARCH",
+                payload: e.target.value,
+              });
+            }}
+          />
+        </Navbar.Text>
+        <Nav>
+          <Dropdown>
+            <Dropdown.Toggle variant="success">
+              <FaShoppingCart color="white" fontSize="25px" />
+              <Badge>{cart.length}</Badge>
+            </Dropdown.Toggle>
+            <Dropdown.Menu style={{ minWidth: 370 }}  align="end">
+            {cart.length > 0 ? (
+                <>
+                  {cart.map((prod) => (
+                    <span className="cartitem" key={prod.id}>
+                      <img
+                        src={prod.image}
+                        className="cartItemImg"
+                        alt={prod.name}
+                      />
+                      <div className="cartItemDetail">
+                        <span>{prod.name}</span>
+                        <span>₹ {prod.price.split(".")[0]}</span>
+                      </div>
+                      <AiFillDelete
+                        fontSize="20px"
+                        style={{ cursor: "pointer" }}
+                        onClick={() =>
+                          dispatch({
+                            type: "REMOVE_FROM_CART",
+                            payload: prod,
+                          })
+                        }
+                      />
+                    </span>
+                  ))}
+                  <Link to="/cart">
+                    <Button style={{ width: "95%", margin: "0 10px" }}>
+                      Go To Cart
+                    </Button>
+                  </Link>
+                </>
+              ) : (
+                <span style={{ padding: 10 }}>Cart is Empty!</span>
+              )}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
